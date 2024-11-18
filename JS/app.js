9407da47f0b8c1f489fe6cdfaa8874c668b424df
BritneySpearsToxic <biondigianfranco67@gmail.com>
2024-11-18 05:42:09 -0300

    as

--- JS/app.js
+++ JS/app.js
@@ -15,7 +15,7 @@ function goToURL(url){
 
 function catDispenser() {
     alert("Michi!");
-    show_image("https://jor1968.github.io/Sitio_Prueba/Imagenes/Michi1.jpg",
+    show_image("https://jor1968.github.io/Mercado_Michi/Imagenes/michi1.jpg",
             300, 200,"Michencio");
         
 }
